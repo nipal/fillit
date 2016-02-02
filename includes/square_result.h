7eb9c7a9cadf7df532baa59ce867f3efdb8ee214
Fabrice JANOTY <fjanoty@e3r12p7.42.fr>
2016-02-03 00:53:37 +0100

    mais oui oui oui

--- includes/square_result.h
+++ includes/square_result.h
@@ -6,7 +6,7 @@
 /*   By: fjanoty <fjanoty@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/05 18:23:53 by fjanoty           #+#    #+#             */
-/*   Updated: 2016/01/27 07:36:32 by fjanoty          ###   ########.fr       */
+/*   Updated: 2016/02/03 00:52:57 by fjanoty          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,30 +29,30 @@ typedef	struct			s_square
 //	incrementation de la taille du taleau						
 //	calcule du plus petit carre possible						
 //	recuperer une ecran de travail----------------------------->	DONE
-//	decaler l'ecran de travail									
-//	placer une piece dans le tableau							
+//	decaler l'ecran de travail--------------------------------->	DONE									
+//	placer une piece dans le tableau--------------------------->	DONE
 //		-la placer dans l'ecran de travail
 //		-ecrire l'ecran dans le tableau
-//	retirer une piece du tableau
+//	retirer une piece du tableau------------------------------->	DONE
 //		-on cheque sur quel area elle est presente
 //		-on genere des masque pour chaque area
 //		-on relance lecran
 //	creation du resultat
 //	decalage de la premier case si pas de solution
 //	liberation de toute la memoire
-//	detection des case vide
+//	detection des case vide (peut etre pas si opti finalement)
 //		-index par type de la dernier place emprinter
 //		-recherche de zone vide puis matchage avec les piece
 
 
 //	OPTIMISATION
-//		-garder en memoir tout les masque possible
+//		-garder en memoir tout les masque possible------------->	DONE
 //		-n'utiliser que des masque verticale (cas particulier?)
 //		-garder en memoir 2 ou 3 ecran ded travaille pour les fin de ligne
 
 //	eran:
-//	on va fionalement utiliser des eccran superposer:
-//	la taille maximum c'est 
+//	on pourai utiliser des eccran superposer:
+//	on aurai donc 3 ecran superposer
 
 
 
